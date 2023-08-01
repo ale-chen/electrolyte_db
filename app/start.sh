e41406dc1a698fe2c20058d1fdf0acb4d1bccfd3
ale-chen <alecchen@uchicago.edu>
2023-08-01 12:57:06 -0500

    start sh changed

--- app/start.sh
+++ app/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python3 -c 'from test import start_server; start_server()'
-chown -R www-data:www-data /app/my_project/app/db
-chmod 666 /app/my_project/app/db/experiment_db.sqlite
+chown -R www-data:www-data ./db
+chmod 666 ./db/experiment_db.sqlite
 uvicorn main:app --host 0.0.0.0 --port 8000
 echo 'start.sh run'
